727d84c87e01ccfa3369b3c8ff74f8d0307b5fac
kdokic <kdokic@umich.edu>
2017-12-02 14:33:07 -0800

    second commit

---
+++ MoveByGaze/Assets/BaseSpatialMappingCollisionDetector.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public abstract class BaseSpatialMappingCollisionDetector : MonoBehaviour
+{
+    public abstract bool CheckIfCanMoveBy(Vector3 delta);
+
+    public abstract Vector3 GetMaxDelta(Vector3 delta);
+}
---
+++ MoveByGaze/Assets/InitialPlaceByTap.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class InitialPlaceByTap : MonoBehaviour, IInputClickHandler
+{
+    protected AudioSource Sound;
+    protected MoveByGaze GazeMover;
+
+    void Start()
+    {
+        Sound = GetComponent<AudioSource>();
+        GazeMover = GetComponent<MoveByGaze>();
+
+        InputManager.Instance.PushFallbackInputHandler(gameObject);
+    }
+
+    public void OnInputClicked(InputEventData eventData)
+    {
+        if (!GazeMover.IsActive)
+        {
+            return;
+        }
+
+        if (Sound != null)
+        {
+            Sound.Play();
+        }
+
+        GazeMover.IsActive = false;
+    }
+
+    void IInputClickHandler.OnInputClicked(InputClickedEventData eventData)
+    {
+        if (!GazeMover.IsActive)
+        {
+            return;
+        }
+
+        if (Sound != null)
+        {
+            Sound.Play();
+        }
+
+        GazeMover.IsActive = false;
+    }
+}
